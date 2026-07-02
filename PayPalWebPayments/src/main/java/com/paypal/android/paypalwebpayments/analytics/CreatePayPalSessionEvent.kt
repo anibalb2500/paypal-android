f31da70017dde11ab5c94b7f83341086e020c205
Anibal Bojorquez <anbojorquez@paypal.com>
2026-07-02 14:07:40 -0700

    formatting

--- PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/analytics/CreatePayPalSessionEvent.kt
+++ PayPalWebPayments/src/main/java/com/paypal/android/paypalwebpayments/analytics/CreatePayPalSessionEvent.kt
@@ -4,8 +4,8 @@ package com.paypal.android.paypalwebpayments.analytics
 
 internal enum class CreatePayPalSessionEvent(val value: String) {
     // @formatter:off
-    STARTED(  "paypal-web-payments:create-paypal-session:started"),
+    STARTED("paypal-web-payments:create-paypal-session:started"),
     SUCCEEDED("paypal-web-payments:create-paypal-session:succeeded"),
-    FAILED(   "paypal-web-payments:create-paypal-session:failed"),
+    FAILED("paypal-web-payments:create-paypal-session:failed"),
     // @formatter:on
 }
